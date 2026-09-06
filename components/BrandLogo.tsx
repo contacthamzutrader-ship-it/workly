8b300021a2ccb540a10c21d8c00154197db7e465
Tahira Shafique <noorinfo308@gmail.com>
2026-09-06 06:06:19 -0700

    fix: enlarge Parwaz logo to make it clearly visible

--- components/BrandLogo.tsx
+++ components/BrandLogo.tsx
@@ -16,7 +16,7 @@ export default function BrandLogo({
       <img
         src="/img/Parwaz.png"
         alt="Parwaz"
-        className={`w-auto object-contain ${compact ? "h-7 sm:h-8" : "h-9 sm:h-11"} ${inverted ? "opacity-95" : ""}`}
+        className={`w-auto object-contain ${compact ? "h-10 sm:h-12" : "h-12 sm:h-16"} ${inverted ? "opacity-95" : ""}`}
       />
     </Link>
   );
