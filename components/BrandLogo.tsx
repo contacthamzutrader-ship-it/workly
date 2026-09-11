536c39ca759362d49805173a2359c1d436d0b624
Tahira Shafique <noorinfo308@gmail.com>
2026-09-11 00:21:33 -0700

    ui: make logo much bigger - md size 200-260px wide in header

--- components/BrandLogo.tsx
+++ components/BrandLogo.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 const sizeClasses: Record<string, string> = {
-  xs: "h-8 min-w-[100px] sm:h-9 sm:min-w-[110px] lg:h-10 lg:min-w-[120px]",
-  sm: "h-10 min-w-[120px] sm:h-11 sm:min-w-[140px]",
-  md: "h-10 min-w-[140px] sm:h-12 sm:min-w-[160px]",
-  lg: "h-12 min-w-[160px] sm:h-14 sm:min-w-[180px]",
+  xs: "h-10 w-[140px] sm:h-11 sm:w-[160px] lg:h-12 lg:w-[180px]",
+  sm: "h-12 w-[180px] sm:h-13 sm:w-[200px] lg:h-14 lg:w-[220px]",
+  md: "h-14 w-[200px] sm:h-15 sm:w-[230px] lg:h-16 lg:w-[260px]",
+  lg: "h-16 w-[240px] sm:h-17 sm:w-[270px] lg:h-18 lg:w-[300px]",
 };
 
 export default function BrandLogo({
@@ -25,7 +25,7 @@ export default function BrandLogo({
       <img
         src="/img/Parwaz.png"
         alt="Parwaz"
-        className={`w-auto object-contain ${sizeClasses[size]} ${inverted ? "opacity-95" : ""}`}
+        className={`object-contain ${sizeClasses[size]} ${inverted ? "opacity-95" : ""}`}
       />
     </Link>
   );
